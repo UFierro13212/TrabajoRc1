01cacfafb5ed28fa4815990f53b2dcc6ee932c4e
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-05-17 16:44:17 -0500

    Agregar variables y arreglos para gestionar platos

---
+++ Problema.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int cantidadPlatos;
+    int i;
+
+    char nombres[5][30];
+    int cantidad[5];
+    float tiempoPorPlato[5];
+    int ingredientesPorPlato[5];
+
+    
