f3cff87b58b58117bd9ab39d8a378aeed12548a9
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-05-17 16:54:04 -0500

    Add files via upload
    Agregar validaciones y captura de datos del sistema de catering

--- Problema.c
+++ Problema.c
@@ -15,4 +15,22 @@ int main() {
     float totalHoras = 0;
     int totalIngredientes = 0;
 
-    
+    printf("SISTEMA DE CATERING Y EVENTOS \n");
+
+    printf("Cuantos tipos de platos va a preparar? (maximo 5): ");
+    scanf("%d", &cantidadPlatos);
+
+    while (cantidadPlatos < 1 || cantidadPlatos > 5) {
+        printf("Error. Ingrese un numero entre 1 y 5: ");
+        scanf("%d", &cantidadPlatos);
+    }
+
+    printf("\nIngrese las horas disponibles del personal: ");
+    scanf("%f", &horasDisponibles);
+
+    printf("Ingrese la cantidad de ingredientes disponibles en bodega: ");
+    scanf("%d", &ingredientesDisponibles);
+
+    printf(" DATOS DE LOS PLATOS \n");
+
+   
