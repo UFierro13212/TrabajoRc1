cd89d9c6e543245dd081c8f769d60e9806a05348
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-05-17 16:57:40 -0500

    Add files via upload
    Implementar ciclo para registrar información de cada plato y calcular el total de horas de preparación e ingredientes requeridos para el evento

--- Problema.c
+++ Problema.c
@@ -15,7 +15,7 @@ int main() {
     float totalHoras = 0;
     int totalIngredientes = 0;
 
-    printf("SISTEMA DE CATERING Y EVENTOS \n");
+    printf("SISTEMA DE CATERING Y EVENTOS ===\n\n");
 
     printf("Cuantos tipos de platos va a preparar? (maximo 5): ");
     scanf("%d", &cantidadPlatos);
@@ -31,6 +31,24 @@ int main() {
     printf("Ingrese la cantidad de ingredientes disponibles en bodega: ");
     scanf("%d", &ingredientesDisponibles);
 
-    printf(" DATOS DE LOS PLATOS \n");
+    printf("\n--- DATOS DE LOS PLATOS ---\n");
 
-   
+    for (i = 0; i < cantidadPlatos; i++) {
+        printf("\nPlato %d\n", i + 1);
+
+        printf("Nombre del plato: ");
+        scanf("%s", nombres[i]);
+
+        printf("Cantidad de platos pedidos: ");
+        scanf("%d", &cantidad[i]);
+
+        printf("Tiempo de preparacion por plato en horas: ");
+        scanf("%f", &tiempoPorPlato[i]);
+
+        printf("Ingredientes necesarios por plato: ");
+        scanf("%d", &ingredientesPorPlato[i]);
+
+        totalHoras = totalHoras + (cantidad[i] * tiempoPorPlato[i]);
+        totalIngredientes = totalIngredientes + (cantidad[i] * ingredientesPorPlato[i]);
+    }
+    
